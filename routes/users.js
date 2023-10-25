ab5d92e790ae7da44ad0157d6bf1b5c6904ddc72
Divya-Sarvepalli <143059445+Divya-Sarvepalli@users.noreply.github.com>
2023-10-25 18:54:04 -0500

    Updated and modified files

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var sum = 0;
+var more = 0;
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more+=1;
+  sum+=more;
+  res.send("Sum is "+sum);
+
 });
 
 module.exports = router;
