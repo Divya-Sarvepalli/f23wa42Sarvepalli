5a518c90bd855bba5e1448a59e373e8f44bad320
Divya-Sarvepalli <143059445+Divya-Sarvepalli@users.noreply.github.com>
2023-10-25 22:17:02 -0500

    updated computation

--- routes/computation.js
+++ routes/computation.js
@@ -7,14 +7,13 @@ router.get('/', function (req, res, next) {
     const search_params = url.searchParams
     if (req.method === 'GET') {
         value = search_params.get("x")
-        if(value === null)
-        {
-            value=Math.round(Math.random()*999);
+        if(value === null){
+            value=Math.round(Math.random()*234);
         }     
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('Math.hypot() applied to ' + value + ' is ' + Math.hypot(2,value));
-        res.write('<br>Math.ceil() applied to ' + value + ' is ' + Math.ceil(value))
-        res.write('<br>Math.clz32() applied to ' + value + ' is ' + Math.clz32(value))
+        res.write('Math.trunc() applied to ' + value + ' is ' + Math.trunc(value));
+        res.write('<br>Math.sign() applied to ' + value + ' is ' + Math.sign(value))
+        res.write('<br>Math.tanh() applied to ' + value + ' is ' + Math.tanh(value))
         res.end()
     }
 
